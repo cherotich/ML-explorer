779fc0748001ae869fc417c71d80e1887b5dae4f
cherotich <carolcaro60@gmail.com>
2021-07-18 16:14:00 +0300

    Data visualization

--- app.py
+++ app.py
@@ -70,6 +70,58 @@ def main():
     if st.checkbox("Summary"):
         st.write(df.describe().T)
     #Plot and visualization
+    st.subheader("Data Visualization")
+    #Correlation
+    #Seaborn
+    #Count Plot
+    #Pie Chart
+    #Customizable plot
+
+    st.subheader("Customizable Plot")
+    all_columns_names = df.columns.tolist()
+    type_of_plot = st.selectbox("Select Type of Plot",["area","bar","line","hist","box","kde"])
+    selected_columns_names = st.multiselect("Select Columns To Plot",all_columns_names)
+    if st.button("Generate Plot"):
+        st.success("Generating Customizable Plot of {} for {}".format(type_of_plot,selected_columns_names))
+        # Plot By Streamlit
+        if type_of_plot == 'area':
+            cust_data = df[selected_columns_names]
+            st.area_chart(cust_data)
+        
+        elif type_of_plot == 'bar':
+            cust_data = df[selected_columns_names]
+            st.bar_chart(cust_data)
+
+       
+        elif type_of_plot == 'line':
+            cust_data = df[selected_columns_names]
+            st.line_chart(cust_data)
+
+       #custom plot
+        elif type_of_plot =='hist' :
+            cust_plot = df[selected_columns_names].plot(kind='hist')
+            st.write(cust_plot)
+            st.pyplot()
+        elif type_of_plot =='box' :
+            cust_plot = df[selected_columns_names].plot(kind='box')
+            st.write(cust_plot)
+            st.pyplot()
+
+        elif type_of_plot == 'kde' :
+            cust_plot = df[selected_columns_names].plot(kind='kde')
+            st.write(cust_plot)
+            st.pyplot()
+
+        # elif type_of_plot :
+        #     cust_plot = df[selected_columns_names].plot(kind=type_of_plot)
+        #     st.write(cust_plot)
+        #     st.pyplot()
+
+        
+        # elif type_of_plot == 'area':
+        #     cust_data = df[selected_columns_names]
+        #     st.area_chart(cust_data)
+
 
 if __name__ =='__main__':
     main()
